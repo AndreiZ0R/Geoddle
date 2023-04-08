52e471d2cd4f0cac543aab7d3c879c30581af562
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-04-08 22:54:02 +0300

    login/signup

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,42 +1,92 @@
 import styles from "./HomePage.module.scss";
 import variables from "../../styles/variables.scss"
 import logo from "../../assets/logo.png";
+import illustration from "../../assets/illustration.jpg";
 import TextField from "../../components/TextField/TextField";
 import FilledButton from "../../components/FilledButton/FilledButton";
 import AppForm from "../../components/AppForm/AppForm";
 import Padding from "../../components/Padding/Padding";
+import {useState} from "react";
 
 export default function HomePage() {
+    const states = {login: "login", signup: "signup"};
+    const loginText = "Not enrolled? Create an account now";
+    const signInText = "Already have an account? Log In";
 
-    const textFields = [
+    const [formState, setFormState] = useState(states.login);
+
+    const loginTextFields = [
         <TextField label="Email" placeholder="Enter your email" type="text" id="email"/>,
-        <TextField label="Password" placeholder="Enter your password" type="password" id="passw"/>];
+        <TextField label="Password" placeholder="Enter your password" type="password" id="passw"/>
+    ];
+
+    const signInTextFields = [
+        <TextField label="Username" placeholder="Enter your username" type="text" id="username"/>,
+        <TextField label="Email" placeholder="Enter your email" type="text" id="email"/>,
+        <TextField label="Password" placeholder="Enter your password" type="password" id="passw"/>
+    ];
+
+    const handleFormState = () => {
+        setFormState(formState === states.login ? states.signup : states.login);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const email = document.querySelector("#email").value;
         const password = document.querySelector("#passw").value;
-        //TODO: sent to api
+
+        if (formState === states.signup) {
+            const username = document.querySelector("#username").value;
+        }
+        //TODO: send to api
     }
 
+    const loginForm = <AppForm
+        onSubmit={handleSubmit}
+        submitButton={
+            <Padding padding={variables.mediumPadding}>
+                <FilledButton label="Log In"/>
+            </Padding>}
+        textFields={loginTextFields.map(textField =>
+            <Padding
+                padding={`${variables.mediumPadding} 0`}>{textField}
+            </Padding>)
+        }/>;
+
+    const signupForm = <AppForm
+        onSubmit={handleSubmit}
+        submitButton={
+            <Padding padding={variables.mediumPadding}>
+                <FilledButton label="Sign Up"/>
+            </Padding>}
+        textFields={signInTextFields.map(textField =>
+            <Padding
+                padding={`${variables.mediumPadding} 0`}>{textField}
+            </Padding>)
+        }/>;
+
+
     return (<div className={styles.wrapper}>
+
         <div className={styles.mainCard}>
 
-            <img src={logo} alt="geoddle" className={styles.logo}/>
+            <div className={styles.half1}>
+                <img src={illustration} alt="explore" className={styles.illustr}/>
+            </div>
+            <div className={styles.half2}>
+                <img src={logo} alt="geoddle" className={styles.logo}/>
 
-            <AppForm
-                onSubmit={handleSubmit}
-                submitButton={
-                    <Padding padding={variables.mediumPadding}>
-                        <FilledButton label="Sign Up"/>
-                    </Padding>}
-                textFields={textFields.map(textField => <Padding
-                    padding={`${variables.mediumPadding} 0`}>{textField}</Padding>)
-                }/>
-            <Padding padding={variables.mediumPadding}>
-                <button className={styles.switchForm}>Already have an account? Log In</button>
-            </Padding>
+                {formState === states.login ? loginForm : signupForm}
+                <Padding padding={variables.mediumPadding}>
+                    <button onClick={handleFormState}
+                            className={styles.switchForm}>{formState === states.login ? loginText : signInText}
+                    </button>
+                </Padding>
+            </div>
+
         </div>
+
+
     </div>);
 
 }
