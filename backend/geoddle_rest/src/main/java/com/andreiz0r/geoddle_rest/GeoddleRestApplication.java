820849a6fe4f8593348de55e5624f7d7b444d2f8
AndreiZ0R <andreiborzagabriel@gmail.com>
2023-04-10 19:17:30 +0300

    backend init

---
+++ backend/geoddle_rest/src/main/java/com/andreiz0r/geoddle_rest/GeoddleRestApplication.java
@@ -0,0 +1,13 @@
+package com.andreiz0r.geoddle_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoddleRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoddleRestApplication.class, args);
+    }
+
+}
---
+++ backend/geoddle_rest/src/test/java/com/andreiz0r/geoddle_rest/GeoddleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreiz0r.geoddle_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoddleRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
